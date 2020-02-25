96cef4942fd3ae7408c95112ced5bfa7cf3dda3c
Jessica <jessica.huang@silkrode.com.tw>
2020-02-25 09:42:08 +0800

    刪除created

--- src/pages/auth/index.vue
+++ src/pages/auth/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="">
     <Btn @onPress="handleGoogleBtnClick"
-         type="primary"
-         block>
+      type="primary"
+      block>
       <fa :icon="['fab', 'google']"
-          class="fa" /> Login with Google
+        class="fa" /> Login with Google
     </Btn>
     <div class="mb-2"></div>
     <Btn @onPress="handleBackBtnClick"
-         type="primary"
-         block>回首頁</Btn>
+      type="primary"
+      block>回首頁</Btn>
   </div>
 </template>
 <script>
@@ -21,7 +21,6 @@ export default {
   components: {
     Btn
   },
-  async created() {},
   computed: {},
   methods: {
     async handleGoogleBtnClick() {
